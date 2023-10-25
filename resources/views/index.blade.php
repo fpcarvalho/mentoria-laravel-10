a8d6c99cd50f135e534c0b65e3d9efe5334ce581
fpcarvalho <fernandopc21@gmail.com>
2023-10-24 21:46:34 -0300

    import do InputMask

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -158,7 +158,12 @@
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.2.1/dist/chart.umd.min.js"></script>
     <script src="/js/dashboard.js"></script>
     <script src="/js/color-modes.js"></script>
+    {{-- InputMask --}}
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.10/jquery.mask.js"></script>
+
+    {{-- BlocUI loading --}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.blockUI/2.70/jquery.blockUI.js"></script>
+    <script src="/js/projeto.js"></script>
 
 </body>
 
--- resources/views/pages/produtos/paginacao.blade.php
+++ resources/views/pages/produtos/paginacao.blade.php
@@ -10,7 +10,9 @@
             <input type="text" name="pesquisar" placeholder="Digite o nome"/>
             <button>Pesquisar</button>
 
-            <a type="button" href="" class="btn btn-success float-end">Incluir Produto</a>
+            <a type="button" href="{{route('cadastrar.produto')}}" class="btn btn-success float-end">
+                Incluir Produto
+            </a>
 
         </form>
 
@@ -37,7 +39,9 @@
                                     <a href="" class="btn btn-light btn-sm">
                                         Editar
                                     </a>
-                                    <a href="{{route('produto.delete')}}" class="btn btn-danger btn-sm">
+
+                                    <meta name="csrf-token" content="{{csrf_token()}}" />
+                                    <a onclick="deleteRegistroPaginacao('{{route('produto.delete')}}', {{$produto->id}})" class="btn btn-danger btn-sm">
                                         Excluir
                                     </a>
                                 </td>
